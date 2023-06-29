7b645420c74addf9ec0c6c5d14447df8d18af8c5
Sunset <18344371003@163.com>
2023-06-29 12:44:31 +0800

    ssm注解开发——springMVC 入门案例之SSM整合：表现层数据封装【重点】：完成
    Signed-off-by: Sunset <18344371003@163.com>

--- javaSSM/SpringMVC/springmvc_ssm/src/main/java/com/song/controller/BookController.java
+++ javaSSM/SpringMVC/springmvc_ssm/src/main/java/com/song/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+//统一每一个控制器方法返回值
 @RestController
 // 声明该类为一个控制器，可以处理HTTP请求并返回相应的结果
 @RequestMapping("/books")
@@ -15,24 +16,33 @@ public class BookController {
     @Autowired
     private BookService bookService;
     @PostMapping
-    public boolean save(@RequestBody Book book) {
-        return bookService.save(book);
+    public Result save(@RequestBody Book book) {
+        boolean flag =  bookService.save(book);
+        return new Result(flag ? Code.SAVE_OK:Code.SAVE_ERR,flag);
     }
     @PutMapping
-    public boolean update(@RequestBody Book book) {
-        return bookService.update(book);
+    public Result update(@RequestBody Book book) {
+        boolean flag =  bookService.update(book);
+        return new Result(flag ? Code.UPDATE_OK:Code.UPDATE_ERR,flag);
     }
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable Integer id) {
-        return bookService.delete(id);
+    public Result delete(@PathVariable Integer id) {
+        boolean flag =  bookService.delete(id);
+        return new Result(flag ? Code.DELETE_OK:Code.DELETE_ERR,flag);
     }
     @GetMapping("/{id}")
-    public Book getById(@PathVariable Integer id) {
-        return bookService.getById(id);
+    public Result getById(@PathVariable Integer id) {
+        Book book =  bookService.getById(id);
+        Integer code = book != null ? Code.GET_OK:Code.GET_ERR;
+        String msg =  book != null ? "":"数据查询失败,请重试！";
+        return new Result(code,book,msg);
     }
     @GetMapping
-    public List<Book> getAll() {
-        return bookService.getAll();
+    public Result getAll() {
+        List<Book> bookList = bookService.getAll();
+        Integer code = bookList != null ? Code.GET_OK : Code.GET_ERR;
+        String msg =  bookList != null ? "":"数据查询失败,请重试！";
+        return new Result(code,bookList,msg);
     }
 
     /**
@@ -53,4 +63,26 @@ public class BookController {
      *
      * 总结：@PathVariable注解用于将请求路径中的占位符参数绑定到方法参数上，以便在方法中进行处理。它是Spring MVC框架中常用的注解之一。
      */
+
+    //@PostMapping
+    //public boolean save(@RequestBody Book book) {
+    //    return bookService.save(book);
+    //}
+    //@PutMapping
+    //public boolean update(@RequestBody Book book) {
+    //    return bookService.update(book);
+    //}
+    //@DeleteMapping("/{id}")
+    //public boolean delete(@PathVariable Integer id) {
+    //    return bookService.delete(id);
+    //}
+    //@GetMapping("/{id}")
+    //public Book getById(@PathVariable Integer id) {
+    //    return bookService.getById(id);
+    //}
+    //@GetMapping
+    //public List<Book> getAll() {
+    //    return bookService.getAll();
+    //}
+
 }
---
+++ javaSSM/SpringMVC/springmvc_ssm/src/main/java/com/song/controller/Result.java
@@ -0,0 +1,49 @@
+package com.song.controller;
+
+public class Result {
+
+    //描述统一格式中的编码，用于区分操作，可以简化配置0或1表示成功失败
+    private Integer code;
+    //描述统一格式中的数据
+    private Object data;
+    //描述统一格式中的消息，可选属性
+    private String msg;
+
+    public Result() {
+    }
+
+    public Result(Integer code, Object data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public Result(Integer code, Object data, String msg) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
