f736a625cd4a7b977b55dd7efe4b2199c314cc99
Sunset <18344371003@163.com>
2023-06-29 23:52:49 +0800

    ssm注解开发——springMVC 入门案例之SSM整合：SSM整合页面开发【重点】:完成100%，对前端页面显示的数据完成了数据的增删改查功能。下一步：拦截器【理解】
    Signed-off-by: Sunset <18344371003@163.com>

--- javaSSM/SpringMVC/springmvc_ssm_exception/src/main/java/com/song/config/springmvcConfig.java
+++ javaSSM/SpringMVC/springmvc_ssm_exception/src/main/java/com/song/config/springmvcConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 // 设置需要扫描的组件的基础包路径，这里指定了com.song.controller包下的所有组件会被扫描并注册到Spring容器中
-@ComponentScan("com.song.controller")
+@ComponentScan({"com.song.controller","com.song.config"})
 // 启用Spring MVC框架的功能 开启json数据类型自动转换
 @EnableWebMvc
 public class springmvcConfig {
--- javaSSM/SpringMVC/springmvc_ssm_exception/src/main/java/com/song/service/impl/BookServiceImpl.java
+++ javaSSM/SpringMVC/springmvc_ssm_exception/src/main/java/com/song/service/impl/BookServiceImpl.java
@@ -18,20 +18,18 @@ public class BookServiceImpl implements BookService {
     private BookDao bookDao;
     @Override
     public boolean save(Book book) {
-        bookDao.save(book);
-        return true;
+
+        return bookDao.save(book)>0;
     }
 
     @Override
     public boolean update(Book book) {
-        bookDao.update(book);
-        return true;
+        return bookDao.save(book)>0;
     }
 
     @Override
     public boolean delete(Integer id) {
-        bookDao.delete(id);
-        return true;
+        return bookDao.delete(id)>0;
     }
 
     @Override
@@ -41,11 +39,11 @@ public class BookServiceImpl implements BookService {
             throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
         }
         //模拟系统异常，将可能出现的异常进行包装，转换成自定义异常
-        try{
-            int i = 1/0;
-        }catch (Exception e){
-            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试!",e);
-        }
+        //try{
+        //    int i = 1/0;
+        //}catch (Exception e){
+        //    throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试!",e);
+        //}
         return bookDao.getById(id);
     }
 
