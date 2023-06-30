68ea3a95caa3993f79a4d303d4a2514710f37e34
Sunset <18344371003@163.com>
2023-06-30 19:42:45 +0800

    ssm注解开发——springMVC 入门案例之SSM整合：拦截器【理解】：完成 下一步 springBoot
    Signed-off-by: Sunset <18344371003@163.com>

---
+++ javaSSM/SpringMVC/springmvc_interceptor/src/main/java/com/song/config/SpringMvcConfig.java
@@ -0,0 +1,28 @@
+package com.song.config;
+
+import com.song.controller.interceptor.ProjectInterceptor;
+import com.song.controller.interceptor.ProjectInterceptor2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan({"com.song.controller"})
+@EnableWebMvc
+//实现WebMvcConfigurer接口可以简化开发，但具有一定的侵入性,跟spring有了强绑定
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private ProjectInterceptor projectInterceptor;
+    @Autowired
+    private ProjectInterceptor2 projectInterceptor2;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //配置多拦截器
+        registry.addInterceptor(projectInterceptor).addPathPatterns("/books","/books/*");
+        registry.addInterceptor(projectInterceptor2).addPathPatterns("/books","/books/*");
+    }
+}
