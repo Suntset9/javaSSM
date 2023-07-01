d80e563a60bc550130c938a95c58a9e1811e2012
Sunset <18344371003@163.com>
2023-07-01 22:28:44 +0800

    ssm注解开发——springBoot: 入门案例，基础配置完成 : 多环境完成：配置文件分类：具体看/resources/config/application.yml文件下注释
    Signed-off-by: Sunset <18344371003@163.com>

---
+++ javaSSM/SpringBoot/springBoot_maven_boot_profile/src/main/java/com/song/SpringBootMavenBootProfileApplication.java
@@ -0,0 +1,13 @@
+package com.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenBootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenBootProfileApplication.class, args);
+    }
+
+}
---
+++ javaSSM/SpringBoot/springBoot_maven_boot_profile/src/test/java/com/song/SpringBootMavenBootProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenBootProfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
